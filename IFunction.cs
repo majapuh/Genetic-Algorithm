5920db4067f90e938f41f45ecc819307e717ea7d
majapuh <mayapuh@gmail.com>
2019-02-22 16:20:32 +0100

    small modifications

--- IFunction.cs
+++ IFunction.cs
@@ -1,16 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace dz4
 {
     public interface IFunction
     {
-       
      double Value(double[] x);
      int Eval();
-
     }
 }
